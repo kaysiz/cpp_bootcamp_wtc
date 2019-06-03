722433e90e6fe7cb2572173aaa0013eaf3e53fad
Kudakwashe SIZIVA <ksiziva@c4r22s4.wethinkcode.co.za>
2019-06-03 17:14:34 +0200

    modify day/00/ex00

--- Day00/ex00/megaphone.cpp
+++ Day00/ex00/megaphone.cpp
@@ -6,35 +6,35 @@
 /*   By: ksiziva <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 14:25:48 by ksiziva           #+#    #+#             */
-/*   Updated: 2019/06/03 14:44:31 by ksiziva          ###   ########.fr       */
+/*   Updated: 2019/06/03 17:12:35 by ksiziva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-void to_upper(char *wrd)
+std::string toUpper(const char *s)
 {
-  int len;
-  int i;
-  
-  i = 0;
-  len = 0;
-
-  while (*wrd)
-  {
-    len++;
+  std::string ret;
+  for (int i = 0; s[i] != '\0'; i++) {
+    ret += std::toupper(s[i]);
   }
+  return ret;
 }
 
 int main(int argc, char **argv)
 {
   int i;
 
-  i = 0;
+  i = 1;
   while (i < argc)
   {
-    std::cout << argv[i] << "\n";
+    std::cout << toUpper(argv[i]);
     i++;
   }
+  if (i == 1) {
+    std::cout << toUpper("* loud and unbearable feedback noise *");
+  }
+  std::cout << "\n";
   return 0;
 }
